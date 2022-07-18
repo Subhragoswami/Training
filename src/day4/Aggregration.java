8be1b7a41211e89dfe4ad70256c58673a2162b20
subhra <subhra.goswami@joshsoftware.com>
2022-07-18 17:57:18 +0530

    commit

---
+++ src/day4/Aggregration.java
@@ -0,0 +1,30 @@
+package day4;
+
+public class Aggregration {
+public static void main(String[] args) {
+	Camera c=new Camera("jpg");
+	Phone p=new Phone(c);
+	p.clickphoto();
+}
+}
+class Camera{
+	String photo;
+	Camera(String g){
+	this.photo=photo;
+	}
+	void click()
+	{
+	System.out.println(photo+" ");
+	}
+}
+class Phone{
+	Camera c;
+	Phone(Camera c){
+	this.c=c;
+	}
+	void clickphoto(){
+	c.click();	
+	}
+}
+
+
---
+++ src/day4/Constructor.java
@@ -0,0 +1,27 @@
+package day4;
+import java.util.Scanner;
+public class Constructor {
+public static void main(String[] args) {
+	Scanner scn=new Scanner(System.in);
+	System.out.println("enter length and width:-");
+	int len=scn.nextInt();
+	int wid=scn.nextInt();
+	Rectangle r=new Rectangle(len,wid);
+	r.display();
+}
+}
+class Rectangle
+{
+	int length;
+	int width;
+	Rectangle(int length,int width)
+	{
+	this.length=length;
+	this.width=width;
+	}
+	
+	void display(){
+	System.out.println("Area of the rectangle is = "+ length*width);
+	}
+}
+
